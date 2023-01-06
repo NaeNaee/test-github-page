7211a5848665de5164661897e875a0e96e89ab01
Nae9Inch <hunterala@gmail.com>
2023-01-06 08:34:17 +0700

    Initial work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useQuery} from "react-query";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const query = useQuery(['animal'], () => fetch('https://apistest-fpipo.md.go.th/Animal').then(res => res.json()))
+    const data: any[] = query.data || [];
+
+    return (
+        <div>
+            <ul>
+                {
+                    data.map(d => (
+                        <li>{d.animalName}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
